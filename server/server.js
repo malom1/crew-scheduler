c4906a0d323558178077b1fcf6eec638fb75d6f7
Mohammad Alom <malom1@fordham.edu>
2025-05-23 12:22:40 -0400

    connect database to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const pdfParse = require('pdf-parse');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./database');
 
 
 const app = express();
@@ -11,4 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// set up storage and uploaded files
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
